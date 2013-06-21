03d1bc6cf64a4bf8de56cc51f1f296815c368897
Jason O'Brien <jason@zenworm.org>
2013-06-21 08:02:34 -0500

    each section has a min-height equal to window height, even on resize

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
-	
+	var $window = $(window),
+		wHeight = $window.height(),
+		wWidth = $window.width();
+
+	$("#hello, #new-album").css("minHeight" , wHeight );
+
+	$(window).resize(function() {
+	    var $window = $(window),
+			wHeight = $window.height(),
+			wWidth = $window.width();
+
+		$("#hello, #new-album").css("minHeight" , wHeight );
+	});
 
 });
