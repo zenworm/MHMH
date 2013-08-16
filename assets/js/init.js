cac9995596856cb383069691a1a6e09a13ca1f08
Jason O'Brien <jason@zenworm.org>
2013-08-16 18:57:39 -0500

    nuking old site/css cleanup

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,17 +1,3 @@
 $(document).ready(function() {
 
-	// var $window = $(window),
-	// 	wHeight = $window.height(),
-	// 	wWidth = $window.width();
-
-	// $("#hello, #new-album").css("minHeight" , wHeight );
-
-	// $(window).resize(function() {
-	//     var $window = $(window),
-	// 		wHeight = $window.height(),
-	// 		wWidth = $window.width();
-
-	// 	$("#hello, #new-album").css("minHeight" , wHeight );
-	// });
-
 });
