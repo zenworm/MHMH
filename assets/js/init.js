2e13df755b86c2908fd3748b68cb6ac815fade1a
Jason O'Brien <jason@zenworm.org>
2013-08-25 19:48:57 -0500

    get rid of a stray console.log

--- assets/js/init.js
+++ assets/js/init.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
   $(".song .lyrics").click(function() {
     var songName = $(this).parents(".song").find("header h1").text();
     var lyricsText = $(this).parents(".song").find(".lyrics-text").html();
-    console.log(songName);
     $(".panel.lyrics .lyrics-text-container").html(lyricsText);
     $(".panel.lyrics header h1").text(songName);
     $(".panel.lyrics").fadeIn(200);
